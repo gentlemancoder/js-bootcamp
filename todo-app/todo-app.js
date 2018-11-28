ade445e62c6ddc86f9c0ea78a10cc9e90958ae6e
chananyajacobson <hananya.jacobson@gmail.com>
2018-11-28 11:41:22 -0500

    Added Filters to todo-app

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,47 +1,68 @@
 const todos = [{
     task: 'wake up',
-    done: true
+    done: false
 }, {
     task: 'get dressed',
-    done: true
+    done: false
 }, {
     task: 'eat breakfast',
     done: false
 }, {
     task: 'go for a walk',
-    done: true
+    done: false
 }, {
     task: 'go to sleep',
     done: false
 }]
 
-// Starts
-const summary = todos.filter(function (todo) {
-    return !todo.done
-})
 
-const todosLeftAnnouncment = document.createElement('h2')
-todosLeftAnnouncment.textContent = `You have ${summary.length} todos left`
-document.querySelector('body').appendChild(todosLeftAnnouncment)
+const filters = {
+    searchText: ''
+}
 
-summary.forEach(function(todo) {
-    console.log('a')
-    const p = document.createElement('p')
-    p.textContent = todo.task
-    document.querySelector('body').appendChild(p)
-})
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        return todo.task.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.done
+    })
+    
+    document.querySelector('#todos').innerHTML = '';
+        
+    const todosLeftAnnouncment = document.createElement('h2')
+    todosLeftAnnouncment.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(todosLeftAnnouncment)
+    
+    filteredTodos.forEach(function (todo){
+        const todoElement = document.createElement('p')
+        todoElement.textContent = todo.task
+        document.querySelector('#todos').appendChild(todoElement)
+    })
 
-// Ends
+    
+}
+
+renderTodos(todos, filters)
+
 
 // Listen for new todo creation
 document.querySelector('#new-todo-text').addEventListener('click', function(e){
     console.log('Add a new todo...')
 })
 
+// Listen for text change
 document.querySelector('#add-todo').addEventListener('input', function(e){
     console.log(e.target.value)
 })
 
+// Listen for filter
+document.querySelector('#search-todos').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+
+})
 
 // let undoneCount = 0
 // todos.forEach(function(todo){
