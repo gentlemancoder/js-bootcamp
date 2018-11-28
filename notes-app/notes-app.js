afec208fd080f80b6513a4c9747b615b08353507
chananyajacobson <hananya.jacobson@gmail.com>
2018-11-28 11:09:39 -0500

    Updated notes-app: added filter

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -9,67 +9,38 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
-const filters = {}
+const filters = {
+    searchText : ''
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note){
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = '';
+    
+    filteredNotes.forEach(function (note){
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
     e.target.textContent = 'The button was clicked'  
 })
 
-document.querySelector('#remove-all').addEventListener('click', function(e){
-    document.querySelectorAll('.note').forEach(function(note){
-        note.remove()
-    })    
-})
-
-document.querySelector('#search-text').addEventListener('input', function(e) {
-    console.log(e.target.value)
-})
-
-// -- Single --
-// p
-// #replace
-// .item
-
-// Multiple
-// p#order
-// button.inventory
-// h#title.application
-// h1.application#title
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// DOM - Document Object Model
-
-// Query all and remove
-// const p =document.querySelector('p')
-// console.log(p)
-
-// const ps = document.querySelectorAll('p')
-// ps.forEach(function (p) {
-//     p.textContent = '*** *** ***'
-//     // console.log(p.textContent)
-//     // p.remove()
+// document.querySelector('#remove-all').addEventListener('click', function(e){
+//     document.querySelectorAll('.note').forEach(function(note){
+//         note.remove()
+//     })    
 // })
 
-// // Add a new element
-// const newParagraph = document.createElement('p')
-// newParagraph.textContent = 'This is a new element from JavaScript'
-// document.querySelector('body').appendChild(newParagraph)
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    filters.searchText = e.target.value
+    renderNotes(notes, filters)
+})
