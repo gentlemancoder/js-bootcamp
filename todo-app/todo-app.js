63640a859246d65e07123f149a947a4879d6c11f
chananyajacobson <hananya.jacobson@gmail.com>
2018-11-28 12:44:59 -0500

    Hide completed filters added to todo-app

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,6 +1,6 @@
 const todos = [{
     task: 'wake up',
-    done: false
+    done: true
 }, {
     task: 'get dressed',
     done: false
@@ -17,36 +17,56 @@ const todos = [{
 
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    hideCompleted: false
 }
 
+
+
 const renderTodos = function (todos, filters) {
+    
     const filteredTodos = todos.filter(function (todo) {
         return todo.task.toLowerCase().includes(filters.searchText.toLowerCase())
     })
-    
+
     const incompleteTodos = filteredTodos.filter(function (todo) {
         return !todo.done
     })
+  
     
     document.querySelector('#todos').innerHTML = '';
-        
-    const todosLeftAnnouncment = document.createElement('h2')
-    todosLeftAnnouncment.textContent = `You have ${incompleteTodos.length} todos left`
-    document.querySelector('#todos').appendChild(todosLeftAnnouncment)
     
-    filteredTodos.forEach(function (todo){
+    if(filters.searchText === '' && !filters.hideCompleted){
+        const unfilteredTodosAnnouncment = document.createElement('h2')
+        unfilteredTodosAnnouncment.textContent = `You have ${todos.length} things to do.`
+        document.querySelector('#todos').appendChild(unfilteredTodosAnnouncment)
+    } else if(filters.searchText !== '' && !filters.hideCompleted) {
+        const filteredTodosAnnouncment = document.createElement('h2')
+        filteredTodosAnnouncment.textContent = `You have ${filteredTodos.length} todos that match this search.`
+        document.querySelector('#todos').appendChild(filteredTodosAnnouncment)
+    } else if(filters.hideCompleted){
+        const todosLeftAnnouncment = document.createElement('h2')
+        todosLeftAnnouncment.textContent = `You have ${incompleteTodos.length} todos left todo`
+        document.querySelector('#todos').appendChild(todosLeftAnnouncment)
+    }
+
+    if(!filters.hideCompleted){
+        filteredTodos.forEach(function (todo){
+            const todoElement = document.createElement('p')
+            todoElement.textContent = todo.task
+            document.querySelector('#todos').appendChild(todoElement)
+        })
+    } else {
+        incompleteTodos.forEach(function (todo){
         const todoElement = document.createElement('p')
         todoElement.textContent = todo.task
         document.querySelector('#todos').appendChild(todoElement)
-    })
-
-    
+        })
+    }
 }
 
 renderTodos(todos, filters)
 
-//
 
 // Listen for filter
 document.querySelector('#search-todos').addEventListener('input', function (e) {
@@ -61,4 +81,9 @@ document.querySelector('#add-todo').addEventListener('submit', function(e){
         done: false})
         renderTodos(todos, filters)
         e.target.elements.newTodo.value = ''
+})
+
+ document.querySelector('#hide-completed').addEventListener('change', function(e){
+    filters.hideCompleted =  e.target.checked
+    renderTodos(todos, filters)
 })
