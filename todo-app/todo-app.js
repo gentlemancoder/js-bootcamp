e190597f1b29df2d991c73dd65f17f0aba03912a
chananyajacobson <hananya.jacobson@gmail.com>
2018-11-28 11:10:15 -0500

    cleaned up id tage

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,27 +15,30 @@ const todos = [{
     done: false
 }]
 
- const summary = todos.filter(function (todo) {
+// Starts
+const summary = todos.filter(function (todo) {
     return !todo.done
 })
 
-const newParagraph = document.createElement('p')
-newParagraph.textContent = `You have ${summary.length} todos left`
-document.querySelector('body').appendChild(newParagraph)
+const todosLeftAnnouncment = document.createElement('h2')
+todosLeftAnnouncment.textContent = `You have ${summary.length} todos left`
+document.querySelector('body').appendChild(todosLeftAnnouncment)
 
 summary.forEach(function(todo) {
     console.log('a')
-    const newParagraph = document.createElement('p')
-    newParagraph.textContent = todo.task
-    document.querySelector('body').appendChild(newParagraph)
+    const p = document.createElement('p')
+    p.textContent = todo.task
+    document.querySelector('body').appendChild(p)
 })
 
+// Ends
+
 // Listen for new todo creation
-document.querySelector('#add-todo').addEventListener('click', function(e){
+document.querySelector('#new-todo-text').addEventListener('click', function(e){
     console.log('Add a new todo...')
 })
 
-document.querySelector('input#add-todo').addEventListener('input', function(e){
+document.querySelector('#add-todo').addEventListener('input', function(e){
     console.log(e.target.value)
 })
 
