b684985d8964b8a99e436f303bf14528ec7303da
chananyajacobson <hananya.jacobson@gmail.com>
2018-11-28 21:29:49 -0500

    refactored notes-app

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,48 +1,19 @@
-let notes = []
+const notes = getSavedNotes()
 
 const filters = {
     searchText : ''
 }
 
-// Check for exisiting saved date
-const notesJSON = localStorage.getItem('notes')
-
-if (notesJSON !== null) {
-    notes = JSON.parse(notesJSON)
-}
-
-const renderNotes = function (notes, filters) {
-    const filteredNotes = notes.filter(function (note){
-        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    })
-
-    document.querySelector('#notes').innerHTML = '';
-    
-    filteredNotes.forEach(function (note){
-        const noteEl = document.createElement('p')
-
-        if (note.title.length > 0 ){
-            noteEl.textContent = note.title
-        } else {
-            noteEl.textContent = 'Unnamed note'
-        }
-
-        document.querySelector('#notes').appendChild(noteEl)
-    })
-}
-
 renderNotes(notes, filters)
-
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
     notes.push({
         title:'',
         body:''
     })
-    localStorage.setItem('notes', JSON.stringify(notes))
+    saveNotes(notes)
     renderNotes(notes, filters)
 })
-
 
 document.querySelector('#search-text').addEventListener('input', function(e) {
     filters.searchText = e.target.value
