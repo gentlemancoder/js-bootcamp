5a316097e4022c61f2a0360f89431cd6eeca8ce8
Chananya Jacobson <chananya.jacobson@gmail.comcd ~notepad .gitignorels ~al ~/.sshgit config --global user.email chananya.jacobson@gmail.com>
2018-12-02 15:01:27 -0500

    makeGuess my way

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -2,7 +2,21 @@
 const Hangman = function (word, remainingGuesses) {
     this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
-    this.lettersGuessed = [' ', 'e']
+    this.lettersGuessed = ['']
+}
+
+Hangman.prototype.makeGuess = function (guess) {
+    if(guess.length > 1){
+        console.log("'Invalid guess")
+    }
+    guess =guess.toLowerCase()
+
+    if (!this.lettersGuessed.includes(guess)) {
+        this.lettersGuessed += guess
+        if (!this.word.includes(guess)){
+            this.remainingGuesses--
+        }
+    }
 }
 
 Hangman.prototype.getPuzzle = function () {
@@ -18,9 +32,14 @@ Hangman.prototype.getPuzzle = function () {
     return puzzle
 }
 
-const game1 = new Hangman('bob', 4)
-
-const game2 = new Hangman('New Jersey', 21)
-
+const game1 = new Hangman('cat', 2)
+game1.makeGuess('c')
+game1.makeGuess('t')
+game1.makeGuess('z')
 console.log(game1.getPuzzle())
+console.log(game1.remainingGuesses)
+console.log(game1.lettersGuessed)
+const game2 = new Hangman('New Jersey', 4)
+game2.makeGuess('w')
 console.log(game2.getPuzzle())
+console.log(game2.remainingGuesses)
