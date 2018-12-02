3396060620805bed2264c4a33d190d100035f8d1
Chananya Jacobson <chananya.jacobson@gmail.comcd ~notepad .gitignorels ~al ~/.sshgit config --global user.email chananya.jacobson@gmail.com>
2018-12-02 14:16:29 -0500

    all about inheritance

--- hangman/app.js
+++ hangman/app.js
@@ -1,10 +1,15 @@
-// product --> Object.prototype --> null
-const product = {
-    name: 'Infulence'
-}
+// Primitive values: string, number, boolean, null, undefined
 
-Object.prototype.someNewMethod = () => 'This is the new function'
+// Object: myObject --> Object.prototype --> null
+// Array: myArray --> Array.prototype --> Object.prototype
+// Function: myFunction --> Function.prototype --> Object.prototype --> null
+// String : myString --> String.prototype --> Object.prototype --> null
+// Number: myNumber --> Number.prototype --> Object.prototype --> null
+// Booloean: myBool --> Boolean.prototype --> Object.prototype --> null
 
-// hasOwnProperty
-console.log(product.hasOwnProperty(hasOwnProperty))
-console.log(product.someNewMethod())
+// Strings numbers and bools are all primitives, but the have object wrappers. i.e. when you call a method on them JS coerces them into objects
+const product = 'Computer'
+console.log(product)
+
+const otherProduct = new String('Phone')
+console.log(otherProduct)
