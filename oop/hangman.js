512cb9b08ff1649e7946f2de6319a7253067c975
Chananya Jacobson <chananya.jacobson@gmail.comcd ~notepad .gitignorels ~al ~/.sshgit config --global user.email chananya.jacobson@gmail.com>
2018-12-02 13:22:22 -0500

    hangman guesses my way

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,11 +1,26 @@
-const Hangman = function (word, guesses) {
-    this.word = word
-    this.guesses = guesses
+
+const Hangman = function (word, remainingGuesses) {
+    this.word = word.toLowerCase().split('')
+    this.remainingGuesses = remainingGuesses
+    this.lettersGuessed = [' ', 'e']
+}
+
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
+    for (let i = 0; i < this.word.length; i++){
+        let isMatch = false
+        for (let j = 0; j < this.lettersGuessed.length; j++) {
+            if (this.word[i] === this.lettersGuessed[j].toLowerCase()) 
+                isMatch = true
+        }
+        puzzle += (isMatch ? this.word[i] : '*')
+    }
+    return puzzle
 }
 
 const game1 = new Hangman('bob', 4)
 
 const game2 = new Hangman('New Jersey', 21)
 
-console.log(game1)
-console.log(game2)
+console.log(game1.getPuzzle())
+console.log(game2.getPuzzle())
