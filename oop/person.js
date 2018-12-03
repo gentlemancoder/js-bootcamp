04221ad35d03fc8488132940097d576cc55c8df9
Chananya Jacobson <chananya.jacobson@gmail.comcd ~notepad .gitignorels ~al ~/.sshgit config --global user.email chananya.jacobson@gmail.com>
2018-12-02 21:12:10 -0500

    converted person to class syntax

--- oop/person.js
+++ oop/person.js
@@ -1,28 +1,31 @@
 // Prototypal Inheritance
 
-const Person = function(firstName, lastName, age, likes=[]) {
-   this.firstName = firstName
-   this.lastName = lastName
-   this.age = age
-   this.likes = likes
-}
+class Person {
+    constructor(firstName, lastName, age, likes=[]) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
+    }
 
-Person.prototype.getBio = function () {
-    let bio = `${this.firstName} is ${this.age}.`
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
     
-    this.likes.forEach((like) => {
-        bio += ` ${this.firstName} likes ${like}.`
-    })
-    
-    return bio
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+        
+        return bio
+    }
+
+    setName(fullName) {
+        const names = fullName.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[1]
+    }
 
 }
 
-Person.prototype.setName = function (fullName) {
-    const names = fullName.split(' ')
-    this.firstName = names[0]
-    this.lastName = names[1]
-}
 
 const me = new Person('Andrew', 'Mead', 27, ['Teaching', 'Biking'])
 
