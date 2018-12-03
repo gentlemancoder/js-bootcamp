7f9d0aff0833ff7333fff75cdbf5f697fcd6b3a1
Chananya Jacobson <chananya.jacobson@gmail.comcd ~notepad .gitignorels ~al ~/.sshgit config --global user.email chananya.jacobson@gmail.com>
2018-12-03 11:22:02 -0500

    addedhttp request

--- hangman/app.js
+++ hangman/app.js
@@ -13,10 +13,23 @@ const game1 = new Hangman('Car Parts', 2)
 puzzleEl.textContent = game1.puzzle
 remainingEl.textContent = game1.remainingGuesses
 
-window.addEventListener('keypress', function (e){
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
     puzzleEl.textContent = game1.puzzle
     remainingEl.textContent = game1.remainingGuesses
     statusEl.textContent = game1.statusMessage
 })
+
+// Making an HTTP request
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', (e) => {
+    if(e.target.readyState === 4){
+        const data = JSON.parse(e.target.responseText);
+        console.log(data)
+    }
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()
