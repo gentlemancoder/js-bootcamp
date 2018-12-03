49b8f57371048199020bce7c823acd404a6565ce
Chananya Jacobson <chananya.jacobson@gmail.comcd ~notepad .gitignorels ~al ~/.sshgit config --global user.email chananya.jacobson@gmail.com>
2018-12-03 13:13:46 -0500

    more closures

--- functions/closures.js
+++ functions/closures.js
@@ -1,11 +1,52 @@
-const myFunction = () => {
-    const message = 'This is my message'
-    const printMessage = () => {
-        console.log(message)
+const createCounter = () => {
+    let count = 0
+
+    return {
+        increment(){
+            count++
+        },
+        decrement(){
+            count --
+        },
+        get(){
+            return count
+        }
     }
-    return printMessage
 }
 
-const myPrintMessage = myFunction()
+const counter = createCounter()
 
-myPrintMessage()
+counter.increment()
+counter.decrement()
+counter.decrement()
+counter.count = 0
+console.log(counter.get())
+
+// Adder
+const createAdder = (a) => {
+    return (b) => {
+        return a+b
+    }
+}
+const add = (a,b) => a+b
+
+const addTen = createAdder(10)
+
+
+const add100 = createAdder(100)
+
+// Tipper
+
+const createTipper = (tipPercent) => {
+    return (bill) => {
+        return bill * tipPercent
+    }
+}
+
+const fifteenPercent = createTipper(.15)
+const twelvePercent = createTipper(.12)
+const twentyPercent = createTipper(.20)
+
+console.log(fifteenPercent(100))
+console.log(twentyPercent(100))
+console.log(twelvePercent(100))
